17fb2b66f47ba132be4693b853d7224b4660321d
ferdousmrym <45340113+ferdousmrym@users.noreply.github.com>
2020-04-02 00:07:50 +0300

    Delete Suc.java

--- Suc.java
+++
@@ -1,8 +0,0 @@
-public class Suc extends Bautura {
-    public int zahar;
-    
-    public Suc(String nume, int pret, int zahar) {
-        super(nume, pret);
-        this.zahar = zahar;
-    }
-}
